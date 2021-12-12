44d073d39926bd48ad17b410911d9f9d5df2a8f6
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-12-12 23:01:06 +0900

    add sql files and sqldef

---
+++ init/create_view.sql
@@ -0,0 +1,3 @@
+create view test.test_view as select name from test.table1;
+create view test2.test_view as select name from test2.table1;
+create view test2.test_view2 as select id, name from test2.table1;
--- init/init.sh
+++ init/init.sh
@@ -4,3 +4,5 @@ CURRENT=$(cd $(dirname $0);pwd)
 
 mysql -uroot -pexample -h 127.0.0.1 < ${CURRENT}/create_db.sql
 mysql -uroot -pexample -h 127.0.0.1 < ${CURRENT}/create_table.sql
+mysql -uroot -pexample -h 127.0.0.1 < ${CURRENT}/insert_data.sql
+mysql -uroot -pexample -h 127.0.0.1 < ${CURRENT}/create_view.sql
