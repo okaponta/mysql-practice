03bd2ac0bbb2f46c53bb82ee061bda7a41ba4235
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-08-21 16:45:26 +0900

    update

--- README.md
+++ README.md
@@ -4,5 +4,11 @@
 ## 全削除
 `docker-compose down --rmi all --volumes`
 
+## 接続
+`mysql -uroot -pexample -h 127.0.0.1`
+
+## sqldef
+`mysqldef -uroot -pexample -h 127.0.0.1 mysql < test.sql`
+
 ## adminer
 http://localhost:8080
--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `table1` (
   `id` int DEFAULT NULL,
-  `name` varchar(10) DEFAULT NULL
+  `name` varchar(10) DEFAULT NULL,
+  `year_field` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE VIEW test_view AS select `test`.`table1`.`id` AS `id`,`test`.`table1`.`name` AS `name` from `test`.`table1`;
